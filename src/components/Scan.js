fe0aac852a438bf6845c38f73f9223ff5db26bdb
Saini-shubham <rj.ss1323@gmail.com>
2023-05-20 01:27:21 -0700

    .

--- src/components/Scan.js
+++ src/components/Scan.js
@@ -1,84 +1,121 @@
-import React, { useCallback, useContext, useEffect, useState } from "react";
-import Scanner from "../components/Scanner/Scanner";
-import { ActionsContext } from "../context";
-import ComponentCard from "./ComponentCard";
+
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import Scanner from '../components/Scanner/Scanner';
+import { ActionsContext } from '../context';
 
 const Scan = () => {
-  const [message, setMessage] = useState("");
-  const [serialNumber, setSerialNumber] = useState("");
-  const { actions, setActions } = useContext(ActionsContext);
+    const [message, setMessage] = useState('');
+    const [serialNumber, setSerialNumber] = useState('');
+    const { actions, setActions} = useContext(ActionsContext);
 
-  const scan = useCallback(async () => {
-    if ("NDEFReader" in window) {
-      try {
-        const ndef = new window.NDEFReader();
-        await ndef.scan();
+    const scan = useCallback(async() => {
+        if ('NDEFReader' in window) { 
+            try {
+                const ndef = new window.NDEFReader();
+                await ndef.scan();
+                
+                console.log("Scan started successfully.");
+                ndef.onreadingerror = () => {
+                    console.log("Cannot read data from the NFC tag. Try another one?");
+                };
+                
+                ndef.onreading = event => {
+                    console.log("NDEF message read.");
+                    onReading(event);
+                    setActions({
+                        scan: 'scanned',
+                        write: null
+                    });
+                };
 
-        console.log("Scan started successfully.");
-        ndef.onreadingerror = () => {
-          console.log("Cannot read data from the NFC tag. Try another one?");
-        };
+            } catch(error){
+                console.log(`Error! Scan failed to start: ${error}.`);
+            };
+        }
+    },[setActions]);
 
-        ndef.onreading = (event) => {
-          console.log("NDEF message read.");
-          onReading(event);
-          setActions({
-            scan: "scanned",
-            write: null,
-          });
-        };
-      } catch (error) {
-        console.log(`Error! Scan failed to start: ${error}.`);
-      }
-    }
-  }, [setActions]);
+    const onReading = ({message, serialNumber}) => {
+        setSerialNumber(serialNumber);
+        for (const record of message.records) {
+            switch (record.recordType) {
+                case "text":
+                    const textDecoder = new TextDecoder(record.encoding);
+                    setMessage(textDecoder.decode(record.data));
+                    break;
+                case "url":
+                    // TODO: Read URL record with record data.
+                    break;
+                default:
+                    // TODO: Handle other records with record data.
+                }
+        }
+    };
 
-  const onReading = ({ serialNumber }) => {
-    setSerialNumber(serialNumber);
-    // for (const record of message.records) {
-    //     switch (record.recordType) {
-    //         case "text":
-    //             const textDecoder = new TextDecoder(record.encoding);
-    //             setMessage(textDecoder.decode(record.data));
-    //             break;
-    //         case "url":
-    //             // TODO: Read URL record with record data.
-    //             break;
-    //         default:
-    //             // TODO: Handle other records with record data.
-    //         }
-    // }
-  };
+    useEffect(() => {
+        scan();
+    }, [scan]);
 
-  useEffect(() => {
-    scan();
-  }, [scan]);
-
-  return (
-    <>
-      {actions.scan === "scanned" ? (
-        // <div>
-        //   <p>Serial Number: {serialNumber}</p>
-        //   <p>Message: {message}</p>
-        //   </div>
-          <ComponentCard
-            title={serialNumber}
-            // subtitle={
-            //   <p>
-            //     5 premium and highly customizable demo variations included in the package, with React
-            //     Router 6, Redux Toolkit, Axios nd much more...
-            //   </p>
-            // }
-          ></ComponentCard>
-        
-      ) : (
-        <Scanner status={actions.scan}></Scanner>
-      )}
-    </>
-  );
+    return(
+        <>
+            {actions.scan === 'scanned' ?  
+            <div>
+                <p>Serial Number: {serialNumber}</p>
+                <p>Message: {message}</p>
+            </div>
+            : <Scanner status={actions.scan}></Scanner> }
+        </>
+    );
 };
 
 export default Scan;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // import React, { useCallback, useContext, useEffect, useState } from 'react';
 // import Scanner from './Scanner/Scanner';
@@ -92,20 +129,21 @@ export default Scan;
 //     //const tagScanStatus= useSelector(state => state.tagDetails.tagScanStatus)
 //     const dispatch = useDispatch()
 
+
 //     const scan = useCallback(async() => {
 //         console.log("scannig started")
-//         if ('NDEFReader' in window) {
+//         if ('NDEFReader' in window) { 
 //             try {
 //                 const ndef = new window.NDEFReader();
 //                 await ndef.scan();
-
+                
 //                 console.log("Scan started successfully.");
 //                 alert("Scan started successfully.");
 //                 ndef.onreadingerror = () => {
 //                     console.log("Cannot read data from the NFC tag. Try another one?");
 //                     //alert("Cannot read data from the NFC tag. Try another one?");
 //                 };
-
+                
 //                 ndef.onreading = event => {
 //                     console.log("NDEF message read.");
 //                     onReading(event);
@@ -147,7 +185,7 @@ export default Scan;
 //     return(
 //         <>
 //             {/* {tagScanStatus=== true ?   */}
-//             {actions === true ?
+//             {actions === true ?  
 //             <div>
 //                 <p>Serial Number: {serialNumber}</p>
 //                 <p>Message: {message}</p>
@@ -158,3 +196,5 @@ export default Scan;
 // };
 
 // export default Scan;
+
+
